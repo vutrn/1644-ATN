adcb903ad595c7c355bbec3090f24cad13d9a57c
vutrn <meoiuwai@gmail.com>
2023-08-20 16:53:52 +0700

    add + edit + delete

--- routes/figure.js
+++ routes/figure.js
@@ -15,13 +15,44 @@ router.get("/detail/:id", async (req, res) => {
   res.render("figure/detail", { figure });
 });
 
+// ----------------------------------------------------------------------------
+
+router.get("/add", (req, res) => {
+  res.render("figure/add");
+});
+
+//nhận & xử lý dữ liệu từ form ADD
+router.post("/add", async (req, res) => {
+  await FigureModel.create(req.body);
+  res.redirect("/figure");
+});
+
+// ----------------------------------------------------------------------------
+
+router.get("/edit/:id", async (req, res) => {
+  const figure = await FigureModel.findById(req.params.id);
+  res.render("figure/edit", { figure });
+});
+
+router.post("/edit/:id", async (req, res) => {
+  await FigureModel.findByIdAndUpdate(req.params.id, req.body);
+  res.redirect("/figure");
+});
+
+// ----------------------------------------------------------------------------
+
+router.get("/delete/:id", async (req, res) => {
+  await FigureModel.findByIdAndDelete(req.params.id);
+  res.redirect("/figure");
+});
+
+// ----------------------------------------------------------------------------
+
 router.post("/search", async (req, res) => {
   var keyword = req.body.keyword;
   const legos = await LegoModel.find({ name: new RegExp(keyword, "i") });
   const figures = await FigureModel.find({ name: new RegExp(keyword, "i") });
   res.render("search-results", { legos, figures });
 });
-
-
 
 module.exports = router;
--- routes/lego.js
+++ routes/lego.js
@@ -14,6 +14,39 @@ router.get("/detail/:id", async (req, res) => {
   res.render("lego/detail", { lego });
 });
 
+// ----------------------------------------------------------------------------
+
+router.get("/add", (req, res) => {
+  res.render("lego/add");
+});
+
+//nhận & xử lý dữ liệu từ form ADD
+router.post("/add", async (req, res) => {
+  await LegoModel.create(req.body);
+  res.redirect("/lego");
+});
+
+// ----------------------------------------------------------------------------
+
+router.get("/edit/:id", async (req, res) => {
+  const lego = await LegoModel.findById(req.params.id);
+  res.render("lego/edit", { lego });
+});
+
+router.post("/edit/:id", async (req, res) => {
+  await LegoModel.findByIdAndUpdate(req.params.id, req.body);
+  res.redirect("/lego");
+});
+
+// ----------------------------------------------------------------------------
+
+router.get("/delete/:id", async (req, res) => {
+  await LegoModel.findByIdAndDelete(req.params.id);
+  res.redirect("/lego");
+});
+
+// ----------------------------------------------------------------------------
+
 router.post("/search", async (req, res) => {
   var keyword = req.body.keyword;
   const legos = await LegoModel.find({ name: new RegExp(keyword, "i") });
